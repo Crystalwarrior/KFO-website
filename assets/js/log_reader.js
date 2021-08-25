43712574f353743c77e2b97869bce719586cefd6
Fantos <k.fantoser@gmail.com>
2021-08-25 03:18:08 +0200

    Implemented use of GM log and currently the only (and best) way it to work. Overhaul of positiong and scaling, no magic number dumbery. Overhaul of html and CSS, better look. Overhaul of namelist, depending on ID it check if namechange happened.

--- assets/js/log_reader.js
+++ assets/js/log_reader.js
@@ -1,13 +1,17 @@
 var logs = {};
+var characters = {};
 var canvas;
 var canvasHeight = 500;
+var firstTime;
 // Area stuff
 var areaCoord = {};
 var areaHeaderY = 10;
 var areaHeaderLeftMar = 0;
-var areaHeaderSpacing = 60;
+var areaHeaderSpacing = 100;
 var areaHeaderBottom = 0;
 var timeColWidth = 100;
+var pointSpacing = 4;
+var pointX = -(areaHeaderSpacing/2) + pointSpacing;
 //SVG stuff
 var svgGroup = {};
 var g;
@@ -15,7 +19,7 @@ var lineg;
 var timeScale
 var blockHeight = 1;
 
-var colours = ['mediumturquoise', 'mediumpurple', 'orange', 'yellow', 'lime', 'green', 'olive', 'maroon', 'salmon', 'mintcream', 'tomato', 'gray', 'violet', 'beige', 'cyan'];
+var colours = ['mediumturquoise', 'mediumpurple', 'orange', 'yellow', 'lime', 'green', 'olive', 'firebrick', 'salmon', 'goldenrod', 'tomato', 'gray', 'violet', 'beige', 'cyan', 'darkseagreen', 'gold', 'lightskyblue', 'pink', 'rebeccapurple', 'lightgreen', 'rosybrown', 'saddlebrown', 'steelblue'];
 
 $(document).ready(function(){
 
@@ -36,8 +40,8 @@ $(document).ready(function(){
       for (let i = 0; i < fileList.length; i++) {
           logs[fileList[i].name.split(".log")[0]] = {}
           logs[fileList[i].name.split(".log")[0]]["file"] = fileList[i]
-          logs[fileList[i].name.split(".log")[0]]["color"] = colours[Object.keys(logs).length-1]
-          $("#nameList").append( "<li><font color='" + colours[Object.keys(logs).length-1] +"'>" + fileList[i].name.split(".log")[0] + "</font></li>" );
+          //logs[fileList[i].name.split(".log")[0]]["color"] = colours[Object.keys(logs).length-1]
+          //$("#nameList").append( "<li><font color='" + colours[Object.keys(logs).length-1] +"'>" + fileList[i].name.split(".log")[0] + "</font></li>" );
         }
       console.log(logs)
   });
@@ -47,7 +51,7 @@ $(document).ready(function(){
     $('#timeline').svg({onLoad: drawInitial});
     areaCoord = {};
     areaHeaderY = 10;
-    areaHeaderBottom = 50;
+    areaHeaderBottom = 10;
     createTimeline()
     //readByLine(logs[$("#name").val()])
     //var time = [$("#fromHour").val(), $("#fromMinute").val(), $("#fromSecond").val()]
@@ -72,10 +76,11 @@ function random(range) {
 
 function drawInitial(svg) {
   canvas = svg;
-  g = svg.group({stroke: 'black'}); 
-  lineg = svg.group({stroke: 'black', strokeWidth: 2});
-  timeScale = canvas.group({stroke: 'black'});
-  travelLines = svg.group({stroke: "blue", transform: 'translate(0,0)', id: "travelLines"}); 
+  timeScale = canvas.group({id: "timeScale", stroke: 'black', transform: "translate(0,"+ 0 +")"});
+  g = svg.group({id: "g", stroke: 'black'}); 
+  linegtwo = svg.group({id: "linegtwo", stroke: 'black', strokeWidth: 2, transform: "translate("+ ((areaHeaderSpacing/2) + timeColWidth) +",0)"});
+  lineg = svg.group({id: "lineg", stroke: 'black', strokeWidth: 2, transform: "translate("+ ((areaHeaderSpacing/2) + timeColWidth) +",0)"});
+  travelLines = svg.group(lineg, {id: "travelLines", stroke: "blue"}); 
   //testPoly = canvas.polyline(travelLines, [[450,250], 
   //  [475,250],[475,220],[500,220],[500,250]], 
   //  {fill: 'none', stroke: 'blue', strokeWidth: 5, id: "polyTest"});
@@ -104,36 +109,15 @@ jQuery.fn.getCoord = function()
   };
 };
 
-function readFile(file) {
-  let reader = new FileReader();
-
-  reader.readAsText(file);
-
-  reader.onload = function() {
-    console.log(reader.result);
-  };
-
-  reader.onerror = function() {
-    console.log(reader.error);
-  };
-}
-
 function createTimeline() {
   var time = [$("#fromHour").val(), $("#fromMinute").val(), $("#fromSecond").val()]
   let now = new Date();
   var fromDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), ...time);
   time = [$("#toHour").val(), $("#toMinute").val(), $("#toSecond").val()]
   var toDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), ...time);
-  var areaHeaderLine = canvas.line(g, 0, areaHeaderBottom, 5000, areaHeaderBottom, {strokeWidth: 5});
-  $("#travelLines").attr("transform", "translate(0,"+ areaHeaderBottom +")")
-  drawTimescale();
-  canvas.line(lineg, timeColWidth-15, 0, timeColWidth-15, 50000);
-  var charX = 20; 
+  $("#travelLines").attr("transform", "translate("+ 0 +","+ areaHeaderBottom +")")
+  var Ypos = 0; 
   for (const [key, value] of Object.entries(logs)) {
-    logs[key+"Y"] = 0;
-    logs[key+"X"] = charX;
-    charX += 5;
-    canvas.polyline($("#travelLines"), [], {fill: 'none', stroke: logs[key]["color"], strokeWidth: 2, id: key + "line"});
     var reader = new FileReader();
     reader.readAsText(logs[key]["file"]);
     reader.onload = function(){
@@ -142,39 +126,44 @@ function createTimeline() {
       for(var line = 0; line < lines.length-1; line++){
         var lineTime = getLineTime(lines[line]);
         var lineDate = getLineDate(lines[line]);
-        //Check if line have timestampt and if it's between the requested timeframe
+        // Check if line have timestampt and if it's between the requested timeframe
         if (lineTime >= fromDate.toLocaleTimeString('en-UK') && getLineTime(lines[line]) <= toDate.toLocaleTimeString('en-UK')) {
           var lineData = readLine(lines[line]);
-          //Check if the line is an IC message or movement
           if (lineData != -1) {
 
             if ("area" in lineData) { // If it's a movement line
-              //console.log(lines[line]);
+              if (firstTime == null) {
+                firstTime = getLineTime(lines[line]);
+                drawTimescale()
+              };
+              if ("ID" in lineData) {
+                if (lineData["ID"] in characters == false) { //Character is not in the list
+                  characters[lineData["ID"]] = {};
+                  characters[lineData["ID"]]["name"] = lineData["character"];
+                  characters[lineData["ID"]]["Xpos"] = pointX;
+                  pointX += pointSpacing;
+                  characters[lineData["ID"]]["prevArea"] = -1;
+                  characters[lineData["ID"]]["color"] = colours[Object.keys(characters).length-1];
+                  $("#nameList").append( "<li><font id= '" + lineData["ID"] + "' color='" + characters[lineData["ID"]]["color"] +"'>[" + lineData["ID"] + "]" + lineData["character"] + "</font></li>" );
+                  canvas.polyline($("#travelLines"), [], {fill: 'none', stroke: characters[lineData["ID"]]["color"], strokeWidth: 2, id: lineData["ID"] + "line"});
+                }
+                // If a new name belong to that ID and it doesn't contain custom, change to that
+                if (characters[lineData["ID"]]["name"] != lineData["character"] && characters[lineData["ID"]]["name"].search("Custom") == -1) {
+                  characters[lineData["ID"]]["name"] == lineData["character"];
+                  $("#nameList #" + lineData["ID"]).html("["+ lineData["ID"] + "]" + lineData["character"]);
+                };
+              }
               if (lineData["area"] in areaCoord == false) { // Area not yet have a collumn
-                areaCoord[lineData["area"]] = Object.keys(areaCoord).length * areaHeaderSpacing + timeColWidth;
-                //var currentText = canvas.text(0, 0, lineData["area"]);
-                //canvas.configure(currentText, {transform: "translate(" + (areaCoord[lineData["area"]] + areaHeaderLeftMar) + ", " + areaHeaderY +") rotate(90)"}, false);
-                canvas.line(lineg, (areaCoord[lineData["area"]] + 47), 0, (areaCoord[lineData["area"]] + 47), 50000);
-                //Add areaname to sticky graph header
+                areaCoord[lineData["area"]] = ((Object.keys(areaCoord).length) * areaHeaderSpacing);
+                canvas.line(linegtwo, areaCoord[lineData["area"]] - (areaHeaderSpacing/2), 0, areaCoord[lineData["area"]] - (areaHeaderSpacing/2), 50000, {stroke: "black"});
                 $("#graphHeader").append('<div id ="' + lineData["area"] + '" class="roomName sticky">' + formatString(lineData["area"]) + '</div><div class="vl"></div>');
                 document.getElementById(lineData["area"]).style.margin = (areaHeaderSpacing/2)-1.45 + "px";
                 if (Object.keys(areaCoord).length == 1) {
-                  document.getElementById(lineData["area"]).style.marginLeft = timeColWidth+15 + "px";
+                  document.getElementById(lineData["area"]).style.marginLeft = ((areaHeaderSpacing/2) + timeColWidth + $("#sidebar").width()) + "px";
                 };
-                //$("#graphHeader #"+lineData["area"]).style.width = areaHeaderSpacing;
-                //$("#"+lineData["area"]).attr("transform", "translate(" + areaCoord[lineData["area"]] + ", 0)")
-                //$("#"+lineData["area"]).animate({transform: "translateX(" + areaCoord[lineData["area"]] + ")"});
-
-                //if (areaHeaderY + currentText.getBBox().width > areaHeaderBottom) {
-                //  areaHeaderBottom = 10 + areaHeaderY + currentText.getBBox().width;
-                //  canvas.configure(areaHeaderLine, {transform: "translate(0,"+ areaHeaderBottom +")"});
-                //  canvas.configure(timeScale, {transform: "translate(0,"+ areaHeaderBottom +")"})
-                //  $("#travelLines").attr("transform", "translate(0,"+ areaHeaderBottom +")")
-                //}
-                if (areaCoord[lineData["area"]] > $("#timeline").width()) { //Check if we don't run out of space, if yes, make the canvas wider
+                if ($("#timeline").width != $("#graphHeader").width()) { // Check if we don't run out of space, if yes, make the canvas wider
                   var canvasWidth = $("#timeline").width();
-                  $("#content").width(canvasWidth + 80);
-                  $("#timeline").width(canvasWidth + 80);
+                  $("#timeline").width($("#graphHeader").width() - $("#sidebar").width());
                   $("#timeline svg").attr("width", $("#timeline").width());
 
                 }
@@ -182,21 +171,23 @@ function createTimeline() {
               // Place new point
               if (prevMoveDate != -1) {
                 var dateDiff = Math.abs(lineDate - prevMoveDate);
-                logs[key+"Y"] += Math.ceil(dateDiff / (1000)) * blockHeight;
+                Ypos += Math.ceil(dateDiff / (1000)) * blockHeight;
               }
-              if ($("#" + key + "line").attr('points') == undefined) {
-                $("#" + key + "line").attr("points"," " + ((areaCoord[lineData["area"]] - (areaHeaderSpacing/2)) + logs[key+"X"]) + ", " + logs[key+"Y"]);
+              if ($("#" + lineData["ID"] + "line").attr('points') == undefined) {
+                $("#" + lineData["ID"] + "line").attr("points"," " + (areaCoord[lineData["area"]] + characters[lineData["ID"]]["Xpos"]) + ", " + Ypos);
               } else {
-                $("#" + key + "line").attr("points", $("#" + key + "line").attr('points') + " " + ((areaCoord[lineData["area"]] - (areaHeaderSpacing/2)) + logs[key+"X"]) + ", " + logs[key+"Y"]);
+                $("#" + lineData["ID"] + "line").attr("points", $("#" + lineData["ID"] + "line").attr('points') + " " + (characters[lineData["ID"]]["prevArea"] + characters[lineData["ID"]]["Xpos"]) + ", " + Ypos);
+                $("#" + lineData["ID"] + "line").attr("points", $("#" + lineData["ID"] + "line").attr('points') + " " + (areaCoord[lineData["area"]] + characters[lineData["ID"]]["Xpos"]) + ", " + Ypos);
               }
-              canvas.circle($("#travelLines"), ((areaCoord[lineData["area"]] - (areaHeaderSpacing/2)) + logs[key+"X"]), logs[key+"Y"], 3, {fill: 'red', 
-              stroke: $("#" + key + "line").attr("stroke"), strokeWidth: 2});
+              canvas.circle($("#travelLines"), (areaCoord[lineData["area"]] + characters[lineData["ID"]]["Xpos"]), Ypos, 3, {fill: 'red', 
+              stroke: $("#" + lineData["ID"] + "line").attr("stroke"), strokeWidth: 2});
               logs[key+"Y"] += blockHeight;
-              if (areaHeaderBottom + logs[key+"Y"]> $("#timeline").height()) {
-                $("#timeline").height(areaHeaderBottom + logs[key+"Y"] + 100);
+              if (areaHeaderBottom + Ypos > $("#timeline").height()) { //If we run out of vertical space, add to height
+                $("#timeline").height(areaHeaderBottom + Ypos + 100);
                 $("#timeline svg").attr("height", $("#timeline").height());
+                $("#content").height($("#timeline").height());
               }
-              //$("#polyTest").attr("points", $("#polyTest").attr("points") + " 100, 300 " + (areaCoord[lineData["area"]] + 25) + ", 350");
+              characters[lineData["ID"]]["prevArea"] = areaCoord[lineData["area"]]
               prevMoveDate = lineDate;
             } else { // If it's a message line
 
@@ -210,9 +201,21 @@ function createTimeline() {
 }
 
 function drawTimescale() {
-  for (let i = 0; i < 1000; i++) {
-    var lineY = i* (60 * blockHeight);
-    canvas.line(timeScale, timeColWidth-80, lineY, 50000, lineY);
+  var time = firstTime.split(":");
+  var Hour = time[0]
+  var Minute = time[1]
+  var Yoffset = (60 - time[2]) * blockHeight;
+  if (Yoffset == 60) {Yoffset = 0} else {Minute++};
+  for (let i = 0; i < 400; i++) {
+    var lineY = areaHeaderBottom + Yoffset + (i* (60 * blockHeight));
+    canvas.line(timeScale, timeColWidth/2+30, lineY, 50000, lineY);
+    //canvas.text(timeScale, (timeColWidth/2)-100, lineY, Hour + ":" + Minute, {fontSize: "5"});
+    canvas.text(timeScale,(timeColWidth/2)-50, lineY+10, Hour + ":" + Minute ,{fontFamily: 'Verdana', fontSize: '0.2em', fill: 'black'});
+    Minute++;
+    if (Minute == 60) {
+      Hour++;
+      Minute = 0;
+    }
   }
 }
 
@@ -261,25 +264,35 @@ function readLine(line) { //Check if message is an IC message and return time, c
       //Check if server sent the message
       if (line.split(" GMT] ")[1].split(": ")[0] == "$H") {
         //Check if it's a movement message
-        if (line.search("Changed to area:") != -1) {
+        if (line.search("leaves to") != -1 || line.search("moves from") != -1 || line.search("Attempting to kick") != -1) {
           lineResult["area"] = line.split("] ")[line.split("] ").length - 1].split(".")[0];
+          //[OOC][V aug. 22 18:24:06 2021 GMT] $H: [27] Chadwick leaves to [20] [20] Town.
+          if (line.search("leaves to") != -1) {
+            if (line.search("leaves to") != -1) {
+              lineResult["character"] = line.split("]")[3].split(" leaves")[0]
+              lineResult["ID"] = line.split("]")[2].split("[")[1];
+            }
+          }
+          if (line.search("moves from") != -1) {
+            lineResult["character"] = line.split("]")[3].split(" moves")[0];
+            lineResult["ID"] = line.split("]")[2].split("[")[1];
+          }
+          if (line.search("Attempting to kick") != -1) {
+            lineResult["character"] = line.split("]")[3].split(" [")[0];
+            lineResult["ID"] = line.split("]")[2].split("[")[1];
+          }
           return lineResult;
         } else {
           return -1;
-        }
+        };
       } else {
         return -1;
-      }
+      };
     }
   }
 }
  function formatString(theString) {
-   console.log(theString);
-   console.log(theString.length);
-   console.log(theString.lastIndexOf(" "));
-   console.log("________________")
   if (theString.lastIndexOf(" ") == theString.length-1) {
-    console.log("FOUND ONE");
     theString = theString.substr(0, theString.length-1);
   }
   return theString.replace(/ /g, "_");
